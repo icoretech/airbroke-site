e9ab2d76aab51fa1e42c083b0e839eed91b4cb4b
Claudio Poli <masterkain@gmail.com>
2023-05-20 00:40:29 +0200

    Test build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  unoptimized: true,
+  reactStrictMode: true,
 }
 
 module.exports = nextConfig
